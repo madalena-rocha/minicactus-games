7a5e3ed6f97035920c23c8e29c2a8429877cfa1a
madalena-rocha <rochamada1997m@gmail.com>
2023-10-31 15:51:35 -0300

    code formatting improvement

--- README.md
+++ README.md
@@ -20,5 +20,5 @@
 
 <a href="https://www.linkedin.com/in/madalena-machado-rocha/" target="_blank"><img src="https://img.shields.io/badge/-LinkedIn-%230077B5?style=for-the-badge&logo=linkedin&logoColor=white" target="_blank"></a>
 <a href="mailto:rochamada1997m@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white" target="_blank"></a>
-<a href="http://discordapp.com/users/827312692905377802" target="_blank"><img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" target="_blank"></a> 
+<a href="http://discordapp.com/users/827312692905377802" target="_blank"><img src="https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white" target="_blank"></a>
 <a href="https://www.instagram.com/madalena.machado.rocha/" target="_blank"><img src="https://img.shields.io/badge/-Instagram-%23E4405F?style=for-the-badge&logo=instagram&logoColor=white" target="_blank"></a>
--- main.js
+++ main.js
@@ -1,80 +1,85 @@
-const navigation = document.getElementById('navigation')
+const navigation = document.getElementById("navigation");
 
 // Ajuste de carregamento da função onScroll
-window.addEventListener('scroll', onScroll)
+window.addEventListener("scroll", onScroll);
 
-onScroll() // quando carregar o arquivo, execute o onScroll
-function onScroll() { // o objetivo da função é gerencial os scrolls da página
-    showNavOnScroll()
-    showBackToTopButtonOnScroll()
+onScroll(); // quando carregar o arquivo, execute o onScroll
+function onScroll() {
+  // o objetivo da função é gerencial os scrolls da página
+  showNavOnScroll();
+  showBackToTopButtonOnScroll();
 
-    activateMenuAtCurrentSection(games)
-    activateMenuAtCurrentSection(news)
-    activateMenuAtCurrentSection(about)
-    activateMenuAtCurrentSection(contact)
+  activateMenuAtCurrentSection(games);
+  activateMenuAtCurrentSection(news);
+  activateMenuAtCurrentSection(about);
+  activateMenuAtCurrentSection(contact);
 }
 
 // Dependendo da seção que tiver vendo na página, adicionar a classe ativa
 function activateMenuAtCurrentSection(section) {
-    const targetLine = scrollY + innerHeight / 2
-    // scrollY + innerHeight / 2 é o cálculo para determinar a posição da linha imaginária
-    
-    // Verificar se o topo da seção passou da linha alvo
-    
-    const sectionTop = section.offsetTop
-    
-    const sectionHeight = section.offsetHeight
-    
-    const sectionTopReachOrPassedTargetline = targetLine >= sectionTop
+  const targetLine = scrollY + innerHeight / 2;
+  // scrollY + innerHeight / 2 é o cálculo para determinar a posição da linha imaginária
 
-    // Verificar se a base da seção está abaixo da linha alvo
+  // Verificar se o topo da seção passou da linha alvo
 
-    const sectionEndsAt = sectionTop + sectionHeight
+  const sectionTop = section.offsetTop;
 
-    const sectionEndPassedTargetline = sectionEndsAt <= targetLine
+  const sectionHeight = section.offsetHeight;
 
-    const sectionBoundaries = sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline
+  const sectionTopReachOrPassedTargetline = targetLine >= sectionTop;
 
-    const sectionId = section.getAttribute('id')
+  // Verificar se a base da seção está abaixo da linha alvo
 
-    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
-    // no menu, procura pelos elementos a, procura pelo atributo href que contenha o atributo id da seção
-    
-    menuElement.classList.remove('active')
-    if (sectionBoundaries) {
-        menuElement.classList.add('active')
-    }
+  const sectionEndsAt = sectionTop + sectionHeight;
+
+  const sectionEndPassedTargetline = sectionEndsAt <= targetLine;
+
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetline && !sectionEndPassedTargetline;
+
+  const sectionId = section.getAttribute("id");
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`);
+  // no menu, procura pelos elementos a, procura pelo atributo href que contenha o atributo id da seção
+
+  menuElement.classList.remove("active");
+  if (sectionBoundaries) {
+    menuElement.classList.add("active");
+  }
 }
 
-function showNavOnScroll() { // o objetivo da função é mostrar o navigation ao fazer o scroll
-    if (scrollY > 0) {
-        navigation.classList.add('scroll')
-
-    } else {
-        navigation.classList.remove('scroll')
-    }
-}
-   
-function showBackToTopButtonOnScroll() { // o objetivo da função é mostrar o BackToTopButton ao fazer o scroll
-    if (scrollY > 550) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
+function showNavOnScroll() {
+  // o objetivo da função é mostrar o navigation ao fazer o scroll
+  if (scrollY > 0) {
+    navigation.classList.add("scroll");
+  } else {
+    navigation.classList.remove("scroll");
+  }
 }
 
-function openMenu() { // a função será executada quando clicar nos botões de abrir e fechar o menu
-    document.body.classList.add('menu-expanded')
+function showBackToTopButtonOnScroll() {
+  // o objetivo da função é mostrar o BackToTopButton ao fazer o scroll
+  if (scrollY > 550) {
+    backToTopButton.classList.add("show");
+  } else {
+    backToTopButton.classList.remove("show");
+  }
+}
+
+function openMenu() {
+  // a função será executada quando clicar nos botões de abrir e fechar o menu
+  document.body.classList.add("menu-expanded");
 }
 
 function closeMenu() {
-    document.body.classList.remove('menu-expanded')
+  document.body.classList.remove("menu-expanded");
 }
 
-ScrollReveal({ // quando iniciar o scroll, os elementos serão revelados de cima para baixo vagarosamente
-    origin: 'top',
-    distance: '30px',
-    duration: 700
+ScrollReveal({
+  // quando iniciar o scroll, os elementos serão revelados de cima para baixo vagarosamente
+  origin: "top",
+  distance: "30px",
+  duration: 700,
 }).reveal(` 
     #games,
     #games header,
@@ -87,4 +92,4 @@ ScrollReveal({ // quando iniciar o scroll, os elementos serão revelados de cima
     #about .content,
     #contact,
     #contact header,
-    #contact .content`)
+    #contact .content`);
